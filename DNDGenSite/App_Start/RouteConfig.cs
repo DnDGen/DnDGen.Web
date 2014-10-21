4b73216176d1117f0e64ab8ea8a7d2be8e043bf3
Karl Speer <kspeer@qualtrax.com>
2014-10-21 16:46:24 -0400

    The site lives!
    * Stubbed out pages for each place
    * Got a rudimentary description on the homepage

--- DNDGenSite/App_Start/RouteConfig.cs
+++ DNDGenSite/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace DNDGenSite
@@ -15,8 +11,8 @@ namespace DNDGenSite
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                url: "{action}",
+                defaults: new { controller = "View", action = "Home" }
             );
         }
     }
---
+++ DNDGenSite/Controllers/ViewController.cs
@@ -0,0 +1,37 @@
+﻿using System.Web.Mvc;
+
+namespace DNDGenSite.Controllers
+{
+    public class ViewController : Controller
+    {
+        [HttpGet]
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Dice()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Equipment()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Character()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Dungeon()
+        {
+            return View();
+        }
+    }
+}
