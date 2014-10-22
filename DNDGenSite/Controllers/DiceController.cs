e588961faa65c6ed8380737433c391637b8eca27
Karl Speer <kspeer@qualtrax.com>
2014-10-22 12:54:24 -0400

    More formatting of dice page
    * Also began the dice controller

---
+++ DNDGenSite/Controllers/DiceController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web.Mvc;
+using D20Dice;
+using Ninject;
+
+namespace DNDGenSite.Controllers
+{
+    public class DiceController : Controller
+    {
+        [Inject]
+        public IDice Dice { get; set; }
+
+        [HttpGet]
+        public JsonResult D2(Int32 quantity)
+        {
+            var roll = Dice.Roll(quantity).d2();
+            return Json(new { roll = roll }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
