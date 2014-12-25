9830c9346f1f99ca0ac2cfa3106b2caa39172367
Karl Speer <cidthecoatrack@gmail.com>
2014-12-25 00:03:29 -0500

    Big things
    * Updated layout of equipment page
    * Updated a buttload of packages
    * Will have an equipmentGen package update coming - found a bug with
    intelligence knowledge categories

--- DNDGenSite/App_Start/NinjectWebCommon.cs
+++ DNDGenSite/App_Start/NinjectWebCommon.cs
@@ -5,9 +5,9 @@ namespace DNDGenSite.App_Start
 {
     using System;
     using System.Web;
-    using D20Dice.Bootstrap;
-    using EquipmentGen.Bootstrap;
+
     using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+
     using Ninject;
     using Ninject.Web.Common;
 
@@ -15,6 +15,9 @@ namespace DNDGenSite.App_Start
     {
         private static readonly Bootstrapper bootstrapper = new Bootstrapper();
 
+        /// <summary>
+        /// Starts the application
+        /// </summary>
         public static void Start()
         {
             DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
@@ -22,11 +25,18 @@ namespace DNDGenSite.App_Start
             bootstrapper.Initialize(CreateKernel);
         }
 
+        /// <summary>
+        /// Stops the application.
+        /// </summary>
         public static void Stop()
         {
             bootstrapper.ShutDown();
         }
 
+        /// <summary>
+        /// Creates the kernel that will manage your application.
+        /// </summary>
+        /// <returns>The created kernel.</returns>
         private static IKernel CreateKernel()
         {
             var kernel = new StandardKernel();
@@ -36,7 +46,6 @@ namespace DNDGenSite.App_Start
                 kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
 
                 RegisterServices(kernel);
-
                 return kernel;
             }
             catch
@@ -46,14 +55,12 @@ namespace DNDGenSite.App_Start
             }
         }
 
+        /// <summary>
+        /// Load your modules or register your services here!
+        /// </summary>
+        /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
-            var diceLoader = new D20DiceModuleLoader();
-            diceLoader.LoadModules(kernel);
-
-            var equipmentGenLoader = new EquipmentGenModuleLoader();
-            equipmentGenLoader.LoadModules(kernel);
-
             kernel.Load<WebModule>();
         }
     }
