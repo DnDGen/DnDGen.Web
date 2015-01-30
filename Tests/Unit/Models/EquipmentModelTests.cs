67380977cdb71dbc8798bd1f0eec1bd4f8b03d1e
Karl Speer <kspeer@qualtrax.com>
2015-01-30 16:10:11 -0500

    [GH14] Google Analytics
    * Also updated gitignore

---
+++ Tests/Unit/Models/EquipmentModelTests.cs
@@ -0,0 +1,26 @@
+﻿using DNDGenSite.Models;
+using NUnit.Framework;
+
+namespace DNDGenSite.Tests.Unit.Models
+{
+    [TestFixture]
+    public class EquipmentModelTests
+    {
+        private EquipmentModel model;
+
+        [SetUp]
+        public void Setup()
+        {
+            model = new EquipmentModel();
+        }
+
+        [Test]
+        public void ModelIsInitialized()
+        {
+            Assert.That(model.MaxTreasureLevel, Is.EqualTo(0));
+            Assert.That(model.MundaneItemTypes, Is.Empty);
+            Assert.That(model.PoweredItemTypes, Is.Empty);
+            Assert.That(model.TreasureTypes, Is.Empty);
+        }
+    }
+}
