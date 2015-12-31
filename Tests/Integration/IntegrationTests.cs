f051d27d49223bbfe620c409bd130fceb1b3f52d
Karl Speer <cidthecoatrack@gmail.com>
2015-12-30 22:03:56 -0500

    [GH151] Deleting e2e tests
    They are preserved in the `e2etests` branch, but mainline no longer uses
    or needs them

--- Tests/Integration/IntegrationTests.cs
+++ Tests/Integration/IntegrationTests.cs
@@ -3,7 +3,6 @@ using DNDGenSite.App_Start;
 using Ninject;
 using NUnit.Framework;
 using RollGen.Bootstrap;
-using System;
 using TreasureGen.Bootstrap;
 
 namespace DNDGenSite.Tests.Integration
@@ -27,34 +26,20 @@ namespace DNDGenSite.Tests.Integration
             characterGenLoader.LoadModules(kernel);
 
             kernel.Load<WebModule>();
-
-            kernel.Inject(this);
         }
 
-        //INFO: The commented-out code below is for troubleshooting issues with the bindings and injecting,
-        //since the SetUp fail message just says that the setup failed, instead of showing the specific ninject issue
-        //Also, the SetupFixture provided by NUnit fires after Ninject, so it can't help here
-
-        //[SetUp]
-        //public void IntegrationTestsSetup()
-        //{
-        //    kernel = new StandardKernel();
-
-        //    var rollGenModuleLoader = new RollGenModuleLoader();
-        //    rollGenModuleLoader.LoadModules(kernel);
-
-        //    var treasureGenModuleLoader = new TreasureGenModuleLoader();
-        //    treasureGenModuleLoader.LoadModules(kernel);
-
-        //    kernel.Inject(this);
-        //}
+        [SetUp]
+        public void IntegrationTestsSetup()
+        {
+            kernel.Inject(this);
+        }
 
         protected T GetNewInstanceOf<T>()
         {
             return kernel.Get<T>();
         }
 
-        protected T GetNewInstanceOf<T>(String name)
+        protected T GetNewInstanceOf<T>(string name)
         {
             return kernel.Get<T>(name);
         }
