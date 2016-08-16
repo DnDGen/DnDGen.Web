3a1a519f0537c470e11b6d277cc32e49224d4f97
Karl Speer <cidthecoatrack@gmail.com>
2016-08-16 17:09:18 -0400

    Separate unit and integration tests

--- DnDGen.Web.Tests.Integration/Properties/AssemblyInfo.cs
+++ DnDGen.Web.Tests.Integration/Properties/AssemblyInfo.cs
@@ -5,15 +5,14 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DnDGen.Web")]
+[assembly: AssemblyTitle("DnDGen.Web.Tests.Integration")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DnDGen.Web")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("DnDGen.Web.Tests.Integration")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: InternalsVisibleTo("DnDGen.Web.Tests.Unit")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -21,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d4652cce-e898-4837-ae55-9b066fa07ef5")]
+[assembly: Guid("722de08a-3185-48fd-b1c7-78eb3a1cde4d")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -30,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
