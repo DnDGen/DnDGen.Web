1ec2435d4a2063e904af7718820495b064fc97a9
Karl Speer <cidthecoatrack@gmail.com>
2024-02-05 16:51:53 -0500

    Working on tests

--- DnDGen.Api.CharacterGen.Tests.Integration/Functions/GenerateCharacterFunctionTests.cs
+++ DnDGen.Api.CharacterGen.Tests.Integration/Functions/GenerateCharacterFunctionTests.cs
@@ -1,6 +1,7 @@
 using DnDGen.Api.CharacterGen.Dependencies;
 using DnDGen.Api.CharacterGen.Functions;
 using DnDGen.Api.CharacterGen.Tests.Integration.Helpers;
+using DnDGen.CharacterGen.Abilities;
 using DnDGen.CharacterGen.Alignments;
 using DnDGen.CharacterGen.CharacterClasses;
 using DnDGen.CharacterGen.Characters;
@@ -572,22 +573,76 @@ namespace DnDGen.Api.CharacterGen.Tests.Integration.Functions
             Assert.That(character.Race.BaseRace, Is.AnyOf(expectedBaseRaces));
         }
 
-        [TestCase(CharacterClassConstants.Barbarian)]
-        [TestCase(CharacterClassConstants.Bard)]
-        [TestCase(CharacterClassConstants.Cleric)]
-        [TestCase(CharacterClassConstants.Druid)]
-        [TestCase(CharacterClassConstants.Fighter)]
-        [TestCase(CharacterClassConstants.Monk)]
-        [TestCase(CharacterClassConstants.Paladin)]
-        [TestCase(CharacterClassConstants.Ranger)]
-        [TestCase(CharacterClassConstants.Rogue)]
-        [TestCase(CharacterClassConstants.Sorcerer)]
-        [TestCase(CharacterClassConstants.Wizard)]
-        [TestCase(CharacterClassConstants.Adept)]
-        [TestCase(CharacterClassConstants.Aristocrat)]
-        [TestCase(CharacterClassConstants.Commoner)]
-        [TestCase(CharacterClassConstants.Expert)]
-        [TestCase(CharacterClassConstants.Warrior)]
+        [TestCase(RaceConstants.BaseRaces.Aasimar)]
+        [TestCase(RaceConstants.BaseRaces.AquaticElf)]
+        [TestCase(RaceConstants.BaseRaces.Azer)]
+        [TestCase(RaceConstants.BaseRaces.BlueSlaad)]
+        [TestCase(RaceConstants.BaseRaces.Bugbear)]
+        [TestCase(RaceConstants.BaseRaces.Centaur)]
+        [TestCase(RaceConstants.BaseRaces.CloudGiant)]
+        [TestCase(RaceConstants.BaseRaces.DeathSlaad)]
+        [TestCase(RaceConstants.BaseRaces.DeepDwarf)]
+        [TestCase(RaceConstants.BaseRaces.DeepHalfling)]
+        [TestCase(RaceConstants.BaseRaces.Derro)]
+        [TestCase(RaceConstants.BaseRaces.Doppelganger)]
+        [TestCase(RaceConstants.BaseRaces.Drow)]
+        [TestCase(RaceConstants.BaseRaces.DuergarDwarf)]
+        [TestCase(RaceConstants.BaseRaces.FireGiant)]
+        [TestCase(RaceConstants.BaseRaces.ForestGnome)]
+        [TestCase(RaceConstants.BaseRaces.FrostGiant)]
+        [TestCase(RaceConstants.BaseRaces.Gargoyle)]
+        [TestCase(RaceConstants.BaseRaces.Githyanki)]
+        [TestCase(RaceConstants.BaseRaces.Githzerai)]
+        [TestCase(RaceConstants.BaseRaces.Gnoll)]
+        [TestCase(RaceConstants.BaseRaces.Goblin)]
+        [TestCase(RaceConstants.BaseRaces.GrayElf)]
+        [TestCase(RaceConstants.BaseRaces.GraySlaad)]
+        [TestCase(RaceConstants.BaseRaces.GreenSlaad)]
+        [TestCase(RaceConstants.BaseRaces.Grimlock)]
+        [TestCase(RaceConstants.BaseRaces.HalfElf)]
+        [TestCase(RaceConstants.BaseRaces.HalfOrc)]
+        [TestCase(RaceConstants.BaseRaces.Harpy)]
+        [TestCase(RaceConstants.BaseRaces.HighElf)]
+        [TestCase(RaceConstants.BaseRaces.HillDwarf)]
+        [TestCase(RaceConstants.BaseRaces.HillGiant)]
+        [TestCase(RaceConstants.BaseRaces.Hobgoblin)]
+        [TestCase(RaceConstants.BaseRaces.HoundArchon)]
+        [TestCase(RaceConstants.BaseRaces.Human)]
+        [TestCase(RaceConstants.BaseRaces.Janni)]
+        [TestCase(RaceConstants.BaseRaces.Kapoacinth)]
+        [TestCase(RaceConstants.BaseRaces.Kobold)]
+        [TestCase(RaceConstants.BaseRaces.KuoToa)]
+        [TestCase(RaceConstants.BaseRaces.LightfootHalfling)]
+        [TestCase(RaceConstants.BaseRaces.Lizardfolk)]
+        [TestCase(RaceConstants.BaseRaces.Locathah)]
+        [TestCase(RaceConstants.BaseRaces.Merfolk)]
+        [TestCase(RaceConstants.BaseRaces.Merrow)]
+        [TestCase(RaceConstants.BaseRaces.MindFlayer)]
+        [TestCase(RaceConstants.BaseRaces.Minotaur)]
+        [TestCase(RaceConstants.BaseRaces.MountainDwarf)]
+        [TestCase(RaceConstants.BaseRaces.Ogre)]
+        [TestCase(RaceConstants.BaseRaces.OgreMage)]
+        [TestCase(RaceConstants.BaseRaces.Orc)]
+        [TestCase(RaceConstants.BaseRaces.Pixie)]
+        [TestCase(RaceConstants.BaseRaces.Rakshasa)]
+        [TestCase(RaceConstants.BaseRaces.RedSlaad)]
+        [TestCase(RaceConstants.BaseRaces.RockGnome)]
+        [TestCase(RaceConstants.BaseRaces.Sahuagin)]
+        [TestCase(RaceConstants.BaseRaces.Satyr)]
+        [TestCase(RaceConstants.BaseRaces.Scorpionfolk)]
+        [TestCase(RaceConstants.BaseRaces.Scrag)]
+        [TestCase(RaceConstants.BaseRaces.StoneGiant)]
+        [TestCase(RaceConstants.BaseRaces.StormGiant)]
+        [TestCase(RaceConstants.BaseRaces.Svirfneblin)]
+        [TestCase(RaceConstants.BaseRaces.TallfellowHalfling)]
+        [TestCase(RaceConstants.BaseRaces.Tiefling)]
+        [TestCase(RaceConstants.BaseRaces.Troglodyte)]
+        [TestCase(RaceConstants.BaseRaces.Troll)]
+        [TestCase(RaceConstants.BaseRaces.WildElf)]
+        [TestCase(RaceConstants.BaseRaces.WoodElf)]
+        [TestCase(RaceConstants.BaseRaces.YuanTiAbomination)]
+        [TestCase(RaceConstants.BaseRaces.YuanTiHalfblood)]
+        [TestCase(RaceConstants.BaseRaces.YuanTiPureblood)]
         public async Task GenerateCharacter_ReturnsCharacter_SetBaseRace(string baseRace)
         {
             var request = RequestHelper.BuildRequest($"?baseRaceRandomizerType=Set&setBaseRace={HttpUtility.UrlEncode(baseRace)}");
@@ -603,8 +658,244 @@ namespace DnDGen.Api.CharacterGen.Tests.Integration.Functions
             Assert.That(character.Alignment.Full, Is.Not.Empty);
             Assert.That(character.Class.Level, Is.AtLeast(1));
             Assert.That(character.Class.Summary, Is.Not.Empty);
-            Assert.That(character.Class.Name, Is.EqualTo(className));
             Assert.That(character.Race.Summary, Is.Not.Empty);
+            Assert.That(character.Race.BaseRace, Is.EqualTo(baseRace));
+        }
+
+        [TestCase(RaceRandomizerTypeConstants.Metarace.AnyMeta, true,
+            RaceConstants.Metaraces.Ghost,
+            RaceConstants.Metaraces.HalfCelestial,
+            RaceConstants.Metaraces.HalfDragon,
+            RaceConstants.Metaraces.HalfFiend,
+            RaceConstants.Metaraces.Lich,
+            RaceConstants.Metaraces.Mummy,
+            RaceConstants.Metaraces.Vampire,
+            RaceConstants.Metaraces.Werebear,
+            RaceConstants.Metaraces.Wereboar,
+            RaceConstants.Metaraces.Wererat,
+            RaceConstants.Metaraces.Weretiger,
+            RaceConstants.Metaraces.Werewolf)]
+        [TestCase(RaceRandomizerTypeConstants.Metarace.AnyMeta, false,
+            RaceConstants.Metaraces.None,
+            RaceConstants.Metaraces.Ghost,
+            RaceConstants.Metaraces.HalfCelestial,
+            RaceConstants.Metaraces.HalfDragon,
+            RaceConstants.Metaraces.HalfFiend,
+            RaceConstants.Metaraces.Lich,
+            RaceConstants.Metaraces.Mummy,
+            RaceConstants.Metaraces.Vampire,
+            RaceConstants.Metaraces.Werebear,
+            RaceConstants.Metaraces.Wereboar,
+            RaceConstants.Metaraces.Wererat,
+            RaceConstants.Metaraces.Weretiger,
+            RaceConstants.Metaraces.Werewolf)]
+        [TestCase(RaceRandomizerTypeConstants.Metarace.GeneticMeta, true,
+            RaceConstants.Metaraces.HalfCelestial,
+            RaceConstants.Metaraces.HalfDragon,
+            RaceConstants.Metaraces.HalfFiend)]
+        [TestCase(RaceRandomizerTypeConstants.Metarace.GeneticMeta, false,
+            RaceConstants.Metaraces.None,
+            RaceConstants.Metaraces.HalfCelestial,
+            RaceConstants.Metaraces.HalfDragon,
+            RaceConstants.Metaraces.HalfFiend)]
+        [TestCase(RaceRandomizerTypeConstants.Metarace.LycanthropeMeta, true,
+            RaceConstants.Metaraces.Werebear,
+            RaceConstants.Metaraces.Wereboar,
+            RaceConstants.Metaraces.Wererat,
+            RaceConstants.Metaraces.Weretiger,
+            RaceConstants.Metaraces.Werewolf)]
+        [TestCase(RaceRandomizerTypeConstants.Metarace.LycanthropeMeta, false,
+            RaceConstants.Metaraces.None,
+            RaceConstants.Metaraces.Werebear,
+            RaceConstants.Metaraces.Wereboar,
+            RaceConstants.Metaraces.Wererat,
+            RaceConstants.Metaraces.Weretiger,
+            RaceConstants.Metaraces.Werewolf)]
+        [TestCase(RaceRandomizerTypeConstants.Metarace.UndeadMeta, true,
+            RaceConstants.Metaraces.Ghost,
+            RaceConstants.Metaraces.Lich,
+            RaceConstants.Metaraces.Mummy,
+            RaceConstants.Metaraces.Vampire)]
+        [TestCase(RaceRandomizerTypeConstants.Metarace.UndeadMeta, false,
+            RaceConstants.Metaraces.None,
+            RaceConstants.Metaraces.Ghost,
+            RaceConstants.Metaraces.Lich,
+            RaceConstants.Metaraces.Mummy,
+            RaceConstants.Metaraces.Vampire)]
+        [TestCase(RaceRandomizerTypeConstants.Metarace.NoMeta, false,
+            RaceConstants.Metaraces.None)]
+        [TestCase(RaceRandomizerTypeConstants.Metarace.NoMeta, true,
+            RaceConstants.Metaraces.None)]
+        public async Task GenerateCharacter_ReturnsCharacter_MetaraceRandomizers(string metaraceRandomizerType, bool force, params string[] expectedMetaraces)
+        {
+            var request = RequestHelper.BuildRequest($"?metaraceRandomizerType={HttpUtility.UrlEncode(metaraceRandomizerType)}&forceMetarace={force}");
+            var response = await function.Run(request, logger);
+            Assert.That(response, Is.InstanceOf<OkObjectResult>());
+
+            var okResult = response as OkObjectResult;
+            Assert.That(okResult.Value, Is.InstanceOf<Character>());
+
+            var character = okResult.Value as Character;
+            Assert.That(character, Is.Not.Null);
+            Assert.That(character.Summary, Is.Not.Empty);
+            Assert.That(character.Alignment.Full, Is.Not.Empty);
+            Assert.That(character.Class.Level, Is.AtLeast(1));
+            Assert.That(character.Class.Summary, Is.Not.Empty);
+            Assert.That(character.Race.Summary, Is.Not.Empty);
+            Assert.That(character.Race.Metarace, Is.AnyOf(expectedMetaraces));
+        }
+
+        [TestCase(RaceConstants.Metaraces.None)]
+        [TestCase(RaceConstants.Metaraces.Ghost)]
+        [TestCase(RaceConstants.Metaraces.HalfCelestial)]
+        [TestCase(RaceConstants.Metaraces.HalfDragon)]
+        [TestCase(RaceConstants.Metaraces.HalfFiend)]
+        [TestCase(RaceConstants.Metaraces.Lich)]
+        [TestCase(RaceConstants.Metaraces.Mummy)]
+        [TestCase(RaceConstants.Metaraces.Vampire)]
+        [TestCase(RaceConstants.Metaraces.Werebear)]
+        [TestCase(RaceConstants.Metaraces.Wereboar)]
+        [TestCase(RaceConstants.Metaraces.Wererat)]
+        [TestCase(RaceConstants.Metaraces.Weretiger)]
+        [TestCase(RaceConstants.Metaraces.Werewolf)]
+        public async Task GenerateCharacter_ReturnsCharacter_SetMetarace(string metarace)
+        {
+            var request = RequestHelper.BuildRequest($"?metaraceRandomizerType=Set&setMetarace={HttpUtility.UrlEncode(metarace)}");
+            var response = await function.Run(request, logger);
+            Assert.That(response, Is.InstanceOf<OkObjectResult>());
+
+            var okResult = response as OkObjectResult;
+            Assert.That(okResult.Value, Is.InstanceOf<Character>());
+
+            var character = okResult.Value as Character;
+            Assert.That(character, Is.Not.Null);
+            Assert.That(character.Summary, Is.Not.Empty);
+            Assert.That(character.Alignment.Full, Is.Not.Empty);
+            Assert.That(character.Class.Level, Is.AtLeast(1));
+            Assert.That(character.Class.Summary, Is.Not.Empty);
+            Assert.That(character.Race.Summary, Is.Not.Empty);
+            Assert.That(character.Race.Metarace, Is.EqualTo(metarace));
+        }
+
+        [TestCase(RaceRandomizerTypeConstants.Metarace.AnyMeta, true,
+            RaceConstants.Metaraces.Ghost,
+            RaceConstants.Metaraces.HalfCelestial,
+            RaceConstants.Metaraces.HalfDragon,
+            RaceConstants.Metaraces.HalfFiend,
+            RaceConstants.Metaraces.Lich,
+            RaceConstants.Metaraces.Mummy,
+            RaceConstants.Metaraces.Vampire,
+            RaceConstants.Metaraces.Werebear,
+            RaceConstants.Metaraces.Wereboar,
+            RaceConstants.Metaraces.Wererat,
+            RaceConstants.Metaraces.Weretiger,
+            RaceConstants.Metaraces.Werewolf)]
+        [TestCase(RaceRandomizerTypeConstants.Metarace.AnyMeta, false,
+            RaceConstants.Metaraces.None,
+            RaceConstants.Metaraces.Ghost,
+            RaceConstants.Metaraces.HalfCelestial,
+            RaceConstants.Metaraces.HalfDragon,
+            RaceConstants.Metaraces.HalfFiend,
+            RaceConstants.Metaraces.Lich,
+            RaceConstants.Metaraces.Mummy,
+            RaceConstants.Metaraces.Vampire,
+            RaceConstants.Metaraces.Werebear,
+            RaceConstants.Metaraces.Wereboar,
+            RaceConstants.Metaraces.Wererat,
+            RaceConstants.Metaraces.Weretiger,
+            RaceConstants.Metaraces.Werewolf)]
+        [TestCase(RaceRandomizerTypeConstants.Metarace.GeneticMeta, true,
+            RaceConstants.Metaraces.HalfCelestial,
+            RaceConstants.Metaraces.HalfDragon,
+            RaceConstants.Metaraces.HalfFiend)]
+        [TestCase(RaceRandomizerTypeConstants.Metarace.GeneticMeta, false,
+            RaceConstants.Metaraces.None,
+            RaceConstants.Metaraces.HalfCelestial,
+            RaceConstants.Metaraces.HalfDragon,
+            RaceConstants.Metaraces.HalfFiend)]
+        [TestCase(RaceRandomizerTypeConstants.Metarace.LycanthropeMeta, true,
+            RaceConstants.Metaraces.Werebear,
+            RaceConstants.Metaraces.Wereboar,
+            RaceConstants.Metaraces.Wererat,
+            RaceConstants.Metaraces.Weretiger,
+            RaceConstants.Metaraces.Werewolf)]
+        [TestCase(RaceRandomizerTypeConstants.Metarace.LycanthropeMeta, false,
+            RaceConstants.Metaraces.None,
+            RaceConstants.Metaraces.Werebear,
+            RaceConstants.Metaraces.Wereboar,
+            RaceConstants.Metaraces.Wererat,
+            RaceConstants.Metaraces.Weretiger,
+            RaceConstants.Metaraces.Werewolf)]
+        [TestCase(RaceRandomizerTypeConstants.Metarace.UndeadMeta, true,
+            RaceConstants.Metaraces.Ghost,
+            RaceConstants.Metaraces.Lich,
+            RaceConstants.Metaraces.Mummy,
+            RaceConstants.Metaraces.Vampire)]
+        [TestCase(RaceRandomizerTypeConstants.Metarace.UndeadMeta, false,
+            RaceConstants.Metaraces.None,
+            RaceConstants.Metaraces.Ghost,
+            RaceConstants.Metaraces.Lich,
+            RaceConstants.Metaraces.Mummy,
+            RaceConstants.Metaraces.Vampire)]
+        [TestCase(RaceRandomizerTypeConstants.Metarace.NoMeta, false,
+            RaceConstants.Metaraces.None)]
+        [TestCase(RaceRandomizerTypeConstants.Metarace.NoMeta, true,
+            RaceConstants.Metaraces.None)]
+        public async Task GenerateCharacter_ReturnsCharacter_AbilitiesRandomizers(string abilitiesRandomizerType, params int[] expectedAbilities)
+        {
+            var queryString = $"?abilitiesRandomizerType={HttpUtility.UrlEncode(abilitiesRandomizerType)}";
+            queryString += "&levelRandomizerType=Set&setLevel=1";
+            queryString += "&baseRaceRandomizerType=Set&setBaseRace=Human";
+            queryString += $"&metaraceRandomizerType={HttpUtility.UrlEncode(RaceRandomizerTypeConstants.Metarace.NoMeta)}";
+
+            var request = RequestHelper.BuildRequest(queryString);
+            var response = await function.Run(request, logger);
+            Assert.That(response, Is.InstanceOf<OkObjectResult>());
+
+            var okResult = response as OkObjectResult;
+            Assert.That(okResult.Value, Is.InstanceOf<Character>());
+
+            var character = okResult.Value as Character;
+            Assert.That(character, Is.Not.Null);
+            Assert.That(character.Summary, Is.Not.Empty);
+            Assert.That(character.Alignment.Full, Is.Not.Empty);
+            Assert.That(character.Class.Level, Is.AtLeast(1));
+            Assert.That(character.Class.Summary, Is.Not.Empty);
+            Assert.That(character.Race.Summary, Is.Not.Empty);
+            Assert.That(character.Abilities, Has.Count.EqualTo(6)
+                .And.ContainKey(AbilityConstants.Strength)
+                .And.ContainKey(AbilityConstants.Constitution)
+                .And.ContainKey(AbilityConstants.Dexterity)
+                .And.ContainKey(AbilityConstants.Intelligence)
+                .And.ContainKey(AbilityConstants.Wisdom)
+                .And.ContainKey(AbilityConstants.Charisma));
+            Assert.That(character.Abilities[AbilityConstants.Strength], Is.AnyOf(expectedAbilities));
+            Assert.That(character.Abilities[AbilityConstants.Constitution], Is.AnyOf(expectedAbilities));
+            Assert.That(character.Abilities[AbilityConstants.Dexterity], Is.AnyOf(expectedAbilities));
+            Assert.That(character.Abilities[AbilityConstants.Intelligence], Is.AnyOf(expectedAbilities));
+            Assert.That(character.Abilities[AbilityConstants.Wisdom], Is.AnyOf(expectedAbilities));
+            Assert.That(character.Abilities[AbilityConstants.Charisma], Is.AnyOf(expectedAbilities));
+        }
+
+        [Test]
+        public async Task GenerateCharacter_ReturnsCharacter_SetAbilities()
+        {
+            var queryString = $"?abilitiesRandomizerType=Set&setMetarace={HttpUtility.UrlEncode(metarace)}";
+            var request = RequestHelper.BuildRequest(queryString);
+            var response = await function.Run(request, logger);
+            Assert.That(response, Is.InstanceOf<OkObjectResult>());
+
+            var okResult = response as OkObjectResult;
+            Assert.That(okResult.Value, Is.InstanceOf<Character>());
+
+            var character = okResult.Value as Character;
+            Assert.That(character, Is.Not.Null);
+            Assert.That(character.Summary, Is.Not.Empty);
+            Assert.That(character.Alignment.Full, Is.Not.Empty);
+            Assert.That(character.Class.Level, Is.AtLeast(1));
+            Assert.That(character.Class.Summary, Is.Not.Empty);
+            Assert.That(character.Race.Summary, Is.Not.Empty);
+            Assert.That(character.Race.Metarace, Is.EqualTo(metarace));
         }
     }
 }
