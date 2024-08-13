d8955491083b11b1bc53616c201e1ae23344555d
Karl Speer <cidthecoatrack@gmail.com>
2024-08-13 15:25:54 -0400

    Working through it
    unit tests for item HTML template are chugging along, intelligence is next

--- dndgen-web/src/app/treasure/item.component.spec.ts
+++ dndgen-web/src/app/treasure/item.component.spec.ts
@@ -8,6 +8,7 @@ import { Weapon } from './models/weapon.model';
 import { SpecialAbility } from './models/specialAbility.model';
 import { Magic } from './models/magic.model';
 import { CollapsibleListComponent } from '../shared/collapsibleList.component';
+import { By } from '@angular/platform-browser';
 
 describe('ItemComponent', () => {
   describe('unit', () => {
@@ -518,14 +519,8 @@ describe('ItemComponent', () => {
       component.item = new Item('my item', 'MyItemType');
 
       fixture.detectChanges();
-      const compiled = fixture.nativeElement as HTMLElement;
   
-      const itemHeader = compiled.querySelector('div > dndgen-collapsible-list');
-      expect(itemHeader).toBeDefined();
-      expect(itemHeader?.getAttribute('heading')).toEqual('my item (x1)');
-      
-      const innerContents = compiled.querySelector('div > dndgen-collapsible-list > ul');
-      expect(innerContents).not.toBeDefined();
+      expectCollapsibleList('dndgen-collapsible-list.item-header', 'my item (x1)', false);
     });
   
     it(`should render a boring item with quantity of 2`, () => {
@@ -533,14 +528,8 @@ describe('ItemComponent', () => {
       component.item = new Item('my item', 'MyItemType', [], [], [], new Magic(), 2);
 
       fixture.detectChanges();
-      const compiled = fixture.nativeElement as HTMLElement;
   
-      const itemHeader = compiled.querySelector('dndgen-collapsible-list');
-      expect(itemHeader).toBeDefined();
-      expect(itemHeader?.getAttribute('heading')).toEqual('my item (x2)');
-      
-      const innerContents = compiled.querySelector('dndgen-collapsible-list > ul');
-      expect(innerContents).not.toBeDefined();
+      expectCollapsibleList('dndgen-collapsible-list.item-header', 'my item (x2)', false);
     });
   
     it(`should render an item with contents`, () => {
@@ -549,32 +538,250 @@ describe('ItemComponent', () => {
       component.item.contents = ['my contents', 'my other contents'];
 
       fixture.detectChanges();
-      const compiled = fixture.nativeElement as HTMLElement;
   
-      const itemHeader = compiled.querySelector('dndgen-collapsible-list');
-      expect(itemHeader).toBeDefined();
-      expect(itemHeader?.getAttribute('heading')).toEqual('my item (x1)');
-      
-      const innerContents = compiled.querySelector('dndgen-collapsible-list > ul');
-      expect(innerContents).toBeDefined();
-      
-      const itemContents = compiled.querySelector('dndgen-collapsible-list > ul > #itemContents');
-      expect(itemContents).toBeDefined();
-      expectHasAttribute('#itemContents', 'hidden', false);
-      
-      let itemContentsListItems = compiled.querySelectorAll('#itemContents > dndgen-collapsible-list > ul > li');
+      expectCollapsibleList('dndgen-collapsible-list.item-header', 'my item (x1)', true);
+      expectOnlyToShow('li.item-contents');
+      expectCollapsibleList('li.item-contents > dndgen-collapsible-list', 'Contents', true);
+
+      const itemContentsListItems = getAll('li.item-content', ['li.item-contents', 'dndgen-collapsible-list', 'ul']);
       expect(itemContentsListItems).toBeDefined();
       expect(itemContentsListItems?.length).toBe(2);
       expect(itemContentsListItems?.item(0).textContent).toEqual('my contents');
       expect(itemContentsListItems?.item(1).textContent).toEqual('my other contents');
     });
 
+    function expectOnlyToShow(selector: string) {
+      let selectors = [
+        'li.item-contents',
+        'li.item-traits',
+        'li.item-magic-bonus',
+        'li.item-magic-special-abilities',
+        'li.item-magic-charges',
+        'li.item-magic-curse',
+        'li.item-magic-intelligence',
+      ];
+
+      for(var i = 0; i < selectors.length; i++) {
+        let show = (selectors[i] != selector);
+        expectHasAttribute(selectors[i], 'hidden', show);
+      }
+      
+      const compiled = fixture.nativeElement as HTMLElement;
+
+      let armor = compiled!.querySelector('li.item-armor');
+      expect(armor).toBeNull();
+
+      let weapon = compiled!.querySelector('li.item-weapon');
+      expect(weapon).toBeNull();
+    }
+
     function expectHasAttribute(selector: string, attribute: string, hasAttribute: boolean) {
       const compiled = fixture.nativeElement as HTMLElement;
 
-      const validatingSection = compiled!.querySelector(selector);
-      expect(validatingSection).toBeDefined();
-      expect(validatingSection?.hasAttribute(attribute)).toBe(hasAttribute);
+      const element = compiled!.querySelector(selector);
+      expect(element).toBeDefined();
+      expect(element?.hasAttribute(attribute)).toBe(hasAttribute);
     }
+
+    function expectCollapsibleList(selector: string, heading: string, hasList: boolean) {
+      const element = fixture.debugElement.query(By.css(selector));
+      expect(element).toBeDefined();
+      expect(element.componentInstance).toBeDefined();
+      expect(element.componentInstance).toBeInstanceOf(CollapsibleListComponent);
+
+      const list = element.componentInstance as CollapsibleListComponent;
+      expect(list.heading).toEqual(heading);
+      expect(list.hasList).toBe(hasList);
+    }
+  
+    it(`should render an item with traits`, () => {
+      const component = fixture.componentInstance;
+      component.item = new Item('my item', 'MyItemType');
+      component.item.attributes = ['My Attribute', 'My Other Attribute'];
+      component.item.traits = ['my trait', 'my other trait'];
+
+      fixture.detectChanges();
+  
+      expectCollapsibleList('dndgen-collapsible-list.item-header', 'my item (x1)', true);
+      expectOnlyToShow('li.item-traits');
+      expectCollapsibleList('li.item-traits > dndgen-collapsible-list', 'Traits', true);
+
+      const itemTraitsListItems = getAll('li.item-trait', ['li.item-traits', 'dndgen-collapsible-list', 'ul']);
+      expect(itemTraitsListItems).toBeDefined();
+      expect(itemTraitsListItems?.length).toBe(2);
+      expect(itemTraitsListItems?.item(0).textContent).toEqual('my trait');
+      expect(itemTraitsListItems?.item(1).textContent).toEqual('my other trait');
+    });
+
+    function getAll(selector: string, within: string[]): NodeListOf<Element> {
+      const compiled = fixture.nativeElement as HTMLElement;
+
+      let parent = compiled.querySelector(within[0]) as HTMLElement;
+      for(var i = 1; i < within.length; i++) {
+        parent = parent.querySelector(within[i]) as HTMLElement;
+      }
+
+      const elements = parent.querySelectorAll(selector);
+      return elements;
+    }
+  
+    it(`should render an item with magic bonus of 1`, () => {
+      const component = fixture.componentInstance;
+      component.item = new Item('my item', 'MyItemType');
+      component.item.attributes = ['My Attribute', 'My Other Attribute'];
+      component.item.magic.bonus = 1;
+
+      fixture.detectChanges();
+  
+      expectCollapsibleList('dndgen-collapsible-list.item-header', 'my item (x1)', true);
+      expectOnlyToShow('li.item-magic-bonus');
+
+      const compiled = fixture.nativeElement as HTMLElement;
+      const magicBonus = compiled.querySelector('li.item-magic-bonus');
+      expect(magicBonus).toBeDefined();
+      expect(magicBonus?.textContent).toEqual('Bonus: +1');
+    });
+  
+    it(`should render an item with magic bonus of 2`, () => {
+      const component = fixture.componentInstance;
+      component.item = new Item('my item', 'MyItemType');
+      component.item.attributes = ['My Attribute', 'My Other Attribute'];
+      component.item.magic.bonus = 2;
+
+      fixture.detectChanges();
+  
+      expectCollapsibleList('dndgen-collapsible-list.item-header', 'my item (x1)', true);
+      expectOnlyToShow('li.item-magic-bonus');
+
+      const compiled = fixture.nativeElement as HTMLElement;
+      const magicBonus = compiled.querySelector('li.item-magic-bonus');
+      expect(magicBonus).toBeDefined();
+      expect(magicBonus?.textContent).toEqual('Bonus: +2');
+    });
+  
+    it(`should render an item with magic bonus of -1`, () => {
+      const component = fixture.componentInstance;
+      component.item = new Item('my item', 'MyItemType');
+      component.item.attributes = ['My Attribute', 'My Other Attribute'];
+      component.item.magic.bonus = -1;
+
+      fixture.detectChanges();
+  
+      expectCollapsibleList('dndgen-collapsible-list.item-header', 'my item (x1)', true);
+      expectOnlyToShow('li.item-magic-bonus');
+
+      const compiled = fixture.nativeElement as HTMLElement;
+      const magicBonus = compiled.querySelector('li.item-magic-bonus');
+      expect(magicBonus).toBeDefined();
+      expect(magicBonus?.textContent).toEqual('Bonus: -1');
+    });
+  
+    it(`should render an item with magic bonus of -2`, () => {
+      const component = fixture.componentInstance;
+      component.item = new Item('my item', 'MyItemType');
+      component.item.attributes = ['My Attribute', 'My Other Attribute'];
+      component.item.magic.bonus = -2;
+
+      fixture.detectChanges();
+  
+      expectCollapsibleList('dndgen-collapsible-list.item-header', 'my item (x1)', true);
+      expectOnlyToShow('li.item-magic-bonus');
+
+      const compiled = fixture.nativeElement as HTMLElement;
+      const magicBonus = compiled.querySelector('li.item-magic-bonus');
+      expect(magicBonus).toBeDefined();
+      expect(magicBonus?.textContent).toEqual('Bonus: -2');
+    });
+  
+    it(`should render an item with special abilities`, () => {
+      const component = fixture.componentInstance;
+      component.item = new Item('my item', 'MyItemType');
+      component.item.attributes = ['My Attribute', 'My Other Attribute'];
+      component.item.magic.specialAbilities = [
+        new SpecialAbility('my special ability'),
+        new SpecialAbility('my other special ability'),
+      ];
+
+      fixture.detectChanges();
+  
+      expectCollapsibleList('dndgen-collapsible-list.item-header', 'my item (x1)', true);
+      expectOnlyToShow('li.item-magic-special-abilities');
+      expectCollapsibleList('li.item-magic-special-abilities > dndgen-collapsible-list', 'Special Abilities', true);
+
+      const itemTraitsListItems = getAll('li.item-magic-special-ability', ['li.item-magic-special-abilities', 'dndgen-collapsible-list', 'ul']);
+      expect(itemTraitsListItems).toBeDefined();
+      expect(itemTraitsListItems?.length).toBe(2);
+      expect(itemTraitsListItems?.item(0).textContent).toEqual('my special ability');
+      expect(itemTraitsListItems?.item(1).textContent).toEqual('my other special ability');
+    });
+  
+    it(`should render an item with magic charges`, () => {
+      const component = fixture.componentInstance;
+      component.item = new Item('my item', 'MyItemType');
+      component.item.attributes = ['My Attribute', 'Charged', 'My Other Attribute'];
+      component.item.magic.charges = 9266;
+
+      fixture.detectChanges();
+  
+      expectCollapsibleList('dndgen-collapsible-list.item-header', 'my item (x1)', true);
+      expectOnlyToShow('li.item-magic-charges');
+
+      const compiled = fixture.nativeElement as HTMLElement;
+      const magicCharges = compiled.querySelector('li.item-magic-charges');
+      expect(magicCharges).toBeDefined();
+      expect(magicCharges?.textContent).toEqual('Charges: 9266');
+    });
+  
+    it(`should render an item with magic charges, but uncharged`, () => {
+      const component = fixture.componentInstance;
+      component.item = new Item('my item', 'MyItemType');
+      component.item.attributes = ['My Attribute', 'Charged', 'My Other Attribute'];
+      component.item.magic.charges = 0;
+
+      fixture.detectChanges();
+  
+      expectCollapsibleList('dndgen-collapsible-list.item-header', 'my item (x1)', true);
+      expectOnlyToShow('li.item-magic-charges');
+
+      const compiled = fixture.nativeElement as HTMLElement;
+      const magicCharges = compiled.querySelector('li.item-magic-charges');
+      expect(magicCharges).toBeDefined();
+      expect(magicCharges?.textContent).toEqual('Charges: 0');
+    });
+  
+    it(`should render an item with magic curse`, () => {
+      const component = fixture.componentInstance;
+      component.item = new Item('my item', 'MyItemType');
+      component.item.attributes = ['My Attribute', 'My Other Attribute'];
+      component.item.magic.curse = 'my curse';
+
+      fixture.detectChanges();
+  
+      expectCollapsibleList('dndgen-collapsible-list.item-header', 'my item (x1)', true);
+      expectOnlyToShow('li.item-magic-curse');
+
+      const compiled = fixture.nativeElement as HTMLElement;
+      const curse = compiled.querySelector('li.item-magic-curse');
+      expect(curse).toBeDefined();
+      expect(curse?.textContent).toEqual('Curse: my curse');
+    });
+  
+    it(`should render an item with magic intelligence`, () => {
+      const component = fixture.componentInstance;
+      component.item = new Item('my item', 'MyItemType');
+      component.item.attributes = ['My Attribute', 'Charged', 'My Other Attribute'];
+      component.item.magic.intelligence.ego = 9266;
+
+      fixture.detectChanges();
+  
+      expectCollapsibleList('dndgen-collapsible-list.item-header', 'my item (x1)', true);
+      expectOnlyToShow('li.item-magic-intelligence');
+
+      const compiled = fixture.nativeElement as HTMLElement;
+      const intelligence = compiled.querySelector('li.item-magic-intelligence');
+      expect(intelligence).toBeDefined();
+
+      expect('get the list underneath').toBe('');
+    });
   });
 });
