b735b425d1f9d8dad2d6641bbe734949d0162263
Karl Speer <cidthecoatrack@gmail.com>
2025-10-21 15:12:15 -0400

    Update SerializationExtensions.cs

--- DnDGen.Api/SerializationExtensions.cs
+++ DnDGen.Api/SerializationExtensions.cs
@@ -6,7 +6,7 @@ namespace DnDGen.Api
     {
         public static ValueTask WriteDnDGenModelAsJsonAsync<T>(this HttpResponseData response, T instance, CancellationToken cancellationToken = default)
         {
-            return response.WriteAsJsonAsync<object>(instance, cancellationToken);
+            return response.WriteAsJsonAsync<object?>(instance, cancellationToken);
         }
     }
 }
